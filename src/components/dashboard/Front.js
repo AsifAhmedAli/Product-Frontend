a1505f2ddd1be015fbf48f30af57d38522c2f0b6
MahumSamar <msamar.bscs19seecs@seecs.edu.pk>
2021-12-09 05:54:02 +0500

    UI changes in dashboard

--- src/components/dashboard/Front.js
+++ src/components/dashboard/Front.js
@@ -10,11 +10,15 @@ export default function Front() {
                     See More
                 </button>
             </div>
-            <div className={`grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full gap-4 items-center justify-center`}>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+            <div
+                className={`grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 w-full gap-4 items-center justify-center`}>
+                <Card value="Youtube Video"/>
+                <Card value="Ads"/>
+                <Card value="Share Posts"/>
+                <Card value="Youtube Video"/>
+                <div className="hidden xl:inline flex items-center justify-between p-6 ml-4">
+                    <i className="far fa-paper-plane bg-blue-300 text-blue-700 p-6 text-3xl rounded-full m-6 mb-12 cursor-pointer"></i>
+                </div>
             </div>
             <div className="flex items-center sm:justify-between mt-4 px-2 -mb-4 justify-center">
                 <h1 className="text-xl font-bold sm:mr-0 mr-4">Premium</h1>
@@ -22,11 +26,15 @@ export default function Front() {
                     See More
                 </button>
             </div>
-            <div className={`grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full gap-4 items-center justify-center ${styles.custom_scroll}`}>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+            <div
+                className={`grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 w-full gap-4 items-center justify-center ${styles.custom_scroll}`}>
+                <Card value="Survey" stars="10"/>
+                <Card value="Data Entry" stars="10"/>
+                <Card value="Task Completed" stars="30"/>
+                <Card value="Playing Games" stars="99"/>
+                <div className="hidden xl:inline flex items-center p-6 ml-4">
+                    <i className="far fa-paper-plane bg-blue-300 text-blue-700 p-6 text-3xl rounded-full m-6 mb-16 cursor-pointer"></i>
+                </div>
             </div>
         </div>
     )
