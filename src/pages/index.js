5ef97f7efe08ebd4cd72cfd8eacc6fd5dcccd766
MahumSamar <msamar.bscs19seecs@seecs.edu.pk>
2021-12-13 21:55:35 +0500

    tailwind css

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import Brands from '../components/home/Brands';
 import Landing from '../components/home/Landing';
 import Navbar from '../components/home/Navbar';
 import Work from '../components/home/Work';
-import styles from '../styles/Home.module.css';
+// import styles from '../styles/Home.module.css';
 import Pricing from '../components/home/Pricing';
 import About from '../components/home/About';
 import Footer from '../components/home/Footer';
 
 export default function Home() {
   return (
-    <main id={`${styles.home}`}>
+    <main className="backgroundColor">
       <Navbar />
       <Landing />
       <Brands />
