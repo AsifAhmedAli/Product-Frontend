494bb28eec638ee97f688264305ca71310d10bc4
MahumSamar <msamar.bscs19seecs@seecs.edu.pk>
2021-12-13 21:55:07 +0500

    navbar

--- src/components/home/Navbar.js
+++ src/components/home/Navbar.js
@@ -1,146 +1,175 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+//
+// function Navbar() {
+//     const [isOpen, setIsOpen] = useState(false);
+//     return (img
+//         <nav className="py-2 px-4 sm:px-0 relative z-50">
+//             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+//                 <div className="flex items-center justify-between h-16">
+//                     <div className="flex-shrink-0">
+//                         <img
+//                             className="h-8 w-8"
+//                             src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+//                             alt="Workflow"
+//                         />
+//                     </div>
+//                     <div className="hidden md:block">
+//                         <div className="ml-10 flex items-baseline space-x-4">
+//                             <a
+//                                 href="#"
+//                                 className=" hover:text-orange-800 text-white px-3 py-2 rounded-md text-sm font-medium"
+//                             >
+//                                 Dashboard
+//                             </a>
+//
+//                             <a
+//                                 href="#"
+//                                 className="text-gray-300 hover:text-orange-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+//                             >
+//                                 Team
+//                             </a>
+//
+//                             <a
+//                                 href="#"
+//                                 className="text-gray-300 hover:text-orange-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+//                             >
+//                                 Projects
+//                             </a>
+//
+//                             <a
+//                                 href="#"
+//                                 className="text-gray-300 hover:text-orange-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+//                             >
+//                                 Calendar
+//                             </a>
+//
+//                             <a
+//                                 href="#"
+//                                 className="text-gray-300 hover:text-orange-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+//                             >
+//                                 Reports
+//                             </a>
+//                         </div>
+//                     </div>
+//                     <button className="hover:bg-transparent hover:text-white transition-colors bg-white text-black px-3 py-1 rounded-md text-sm font-medium border-white border-opacity-50 border-2">Lets Talk</button>
+//                     <div className="-mr-2 flex md:hidden">
+//                         <button
+//                             onClick={() => setIsOpen(!isOpen)}
+//                             type="button"
+//                             className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+//                             aria-controls="mobile-menu"
+//                             aria-expanded="false"
+//                         >
+//                             <span className="sr-only">Open main menu</span>
+//
+//                             <svg
+//                                 className="block h-6 w-6"
+//                                 xmlns="http://www.w3.org/2000/svg"
+//                                 fill="none"
+//                                 viewBox="0 0 24 24"
+//                                 stroke="currentColor"
+//                                 aria-hidden="true"
+//                             >
+//                                 <path
+//                                     strokeLinecap="round"
+//                                     strokeLinejoin="round"
+//                                     strokeWidth="2"
+//                                     d="M4 6h16M4 12h16M4 18h16"
+//                                 />
+//                             </svg>
+//
+//                             <svg
+//                                 className="hidden h-6 w-6"
+//                                 xmlns="http://www.w3.org/2000/svg"
+//                                 fill="none"
+//                                 viewBox="0 0 24 24"
+//                                 stroke="currentColor"
+//                                 aria-hidden="true"
+//                             >
+//                                 <path
+//                                     strokeLinecap="round"
+//                                     strokeLinejoin="round"
+//                                     strokeWidth="2"
+//                                     d="M6 18L18 6M6 6l12 12"
+//                                 />
+//                             </svg>
+//                         </button>
+//                     </div>
+//                 </div>
+//             </div>
+//
+//             {isOpen && (
+//                 <div className="md:hidden transition-all" id="mobile-menu">
+//                     <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+//                         <a
+//                             href="#"
+//                             className="hover:text-orange-800 text-white block px-3 py-2 rounded-md text-base font-medium"
+//                         >
+//                             Dashboard
+//                         </a>
+//
+//                         <a
+//                             href="#"
+//                             className="text-gray-300 hover:text-orange-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+//                         >
+//                             Team
+//                         </a>
+//
+//                         <a
+//                             href="#"
+//                             className="text-gray-300 hover:text-orange-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+//                         >
+//                             Projects
+//                         </a>
+//
+//                         <a
+//                             href="#"
+//                             className="text-gray-300 hover:text-orange-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+//                         >
+//                             Calendar
+//                         </a>
+//
+//                         <a
+//                             href="#"
+//                             className="text-gray-300 hover:text-orange-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+//                         >
+//                             Reports
+//                         </a>
+//                     </div>
+//                 </div>
+//             )}
+//
+//         </nav>
+//     );
+// }
+// export default Navbar;
+//
+import Image from "next/image"
 
 function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
-    return (
-        <nav className="py-2 px-4 sm:px-0 relative z-50">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-16">
-                    <div className="flex-shrink-0">
-                        <img
-                            className="h-8 w-8"
-                            src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                            alt="Workflow"
-                        />
-                    </div>
-                    <div className="hidden md:block">
-                        <div className="ml-10 flex items-baseline space-x-4">
-                            <a
-                                href="#"
-                                className=" hover:text-orange-800 text-white px-3 py-2 rounded-md text-sm font-medium"
-                            >
-                                Dashboard
-                            </a>
-
-                            <a
-                                href="#"
-                                className="text-gray-300 hover:text-orange-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                            >
-                                Team
-                            </a>
-
-                            <a
-                                href="#"
-                                className="text-gray-300 hover:text-orange-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                            >
-                                Projects
-                            </a>
-
-                            <a
-                                href="#"
-                                className="text-gray-300 hover:text-orange-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                            >
-                                Calendar
-                            </a>
-
-                            <a
-                                href="#"
-                                className="text-gray-300 hover:text-orange-800 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                            >
-                                Reports
-                            </a>
-                        </div>
-                    </div>
-                    <button className="hover:bg-transparent hover:text-white transition-colors bg-white text-black px-3 py-1 rounded-md text-sm font-medium border-white border-opacity-50 border-2">Lets Talk</button>
-                    <div className="-mr-2 flex md:hidden">
-                        <button
-                            onClick={() => setIsOpen(!isOpen)}
-                            type="button"
-                            className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                            aria-controls="mobile-menu"
-                            aria-expanded="false"
-                        >
-                            <span className="sr-only">Open main menu</span>
-
-                            <svg
-                                className="block h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h16M4 18h16"
-                                />
-                            </svg>
-
-                            <svg
-                                className="hidden h-6 w-6"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-            </div>
-
-            {isOpen && (
-                <div className="md:hidden transition-all" id="mobile-menu">
-                    <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                        <a
-                            href="#"
-                            className="hover:text-orange-800 text-white block px-3 py-2 rounded-md text-base font-medium"
-                        >
-                            Dashboard
-                        </a>
-
-                        <a
-                            href="#"
-                            className="text-gray-300 hover:text-orange-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                        >
-                            Team
-                        </a>
-
-                        <a
-                            href="#"
-                            className="text-gray-300 hover:text-orange-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                        >
-                            Projects
-                        </a>
-
-                        <a
-                            href="#"
-                            className="text-gray-300 hover:text-orange-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                        >
-                            Calendar
-                        </a>
-
-                        <a
-                            href="#"
-                            className="text-gray-300 hover:text-orange-800 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                        >
-                            Reports
-                        </a>
-                    </div>
-                </div>
-            )}
-
-        </nav>
-    );
+  return (
+    <div className="flex justify-between w-full p-4 space-x-6">
+      <div className="relative w-36 h-12">
+        <Image
+          src="https://images.unsplash.com/photo-1496200186974-4293800e2c20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"
+          alt="logo image"
+          layout="fill"
+          objectFit="contain"
+        />
+      </div>
+      <div className="flex items-center justify-between textColorBlue px-4">
+        <p className="navLink">Home</p>
+        <p className="navLink">About</p>
+        <p className="navLink">What We Do</p>
+        <p className="navLink">How To Earn</p>
+        <p className="navLink">Pricing</p>
+        <p className="navLink">Contact Us</p>
+      </div>
+      <div className="flex items-center justify-between space-x-2 p-4 border rounded-full border-blue-800 textColorBlue">
+        <button className="px-4">LOGIN</button>
+        <i className="fas fa-arrow-right"></i>
+      </div>
+    </div>
+  )
 }
-export default Navbar;
-
+export default Navbar
