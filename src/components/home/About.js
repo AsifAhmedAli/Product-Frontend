bb87635bd627ea06f7deb950ca3edac915c001bb
MahumSamar <msamar.bscs19seecs@seecs.edu.pk>
2021-12-15 07:32:26 +0500

    about section

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,20 +1,60 @@
-import HomeBtn from '../../elements/HomeBtn';
-import styles from '../../styles/Home.module.css';
+// import HomeBtn from '../../elements/HomeBtn';
+// import styles from '../../styles/Home.module.css';
 
-export default function About() {
-    return (
-        <article className={` mt-10 mb-20`}>
-            <div className={`${styles.section_gradient} container text-white mx-auto py-10 px-8`}>
-                <div className="flex items-center sm:justify-between justify-center flex-wrap">
-                    <h1 className="text-6xl capitalize font-bold font_raleway">About us</h1>
-                    <div className="sm:w-1/2 w-full sm:px-0 px-4 py-8 sm:py-0 text-center sm:text-left">
-                        <p className="mb-8">Leverage agile frameworks to provide a robust synopsis for high level overviews.
-                            Iterative approaches to corporate strategy foster collaborative thinking to further
-                            the overall value proposition.</p>
-                        <HomeBtn>Learn More</HomeBtn>
-                    </div>
-                </div>
-            </div>
-        </article>
-    )
+import HomeBtn from "../../elements/HomeBtn"
+
+// export default function About() {
+//     return (
+//         <article className={` mt-10 mb-20`}>
+//             <div className={`${styles.section_gradient} container text-white mx-auto py-10 px-8`}>
+//                 <div className="flex items-center sm:justify-between justify-center flex-wrap">
+//                     <h1 className="text-6xl capitalize font-bold font_raleway">About us</h1>
+//                     <div className="sm:w-1/2 w-full sm:px-0 px-4 py-8 sm:py-0 text-center sm:text-left">
+//                         <p className="mb-8">Leverage agile frameworks to provide a robust synopsis for high level overviews.
+//                             Iterative approaches to corporate strategy foster collaborative thinking to further
+//                             the overall value proposition.</p>
+//                         <HomeBtn>Learn More</HomeBtn>
+//                     </div>
+//                 </div>
+//             </div>
+//         </article>
+//     )
+// }
+function About() {
+  return (
+    <div className="about my-24">
+      <div className="grid grid-cols-1 lg:grid-cols-3 px-10 py-6 gap-4">
+        {/* grid */}
+        <div className="flex flex-col items-center justify-between">
+          <p className="textHeading mb-4">About Kamaobay</p>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem
+          </p>
+        </div>
+        <div className="grid grid-cols-1 my-auto lg:grid-cols-3 gap-4 col-span-2">
+          <input
+            className="aboutInput"
+            type="text"
+            placeholder="Name"
+            name=""
+            id=""
+          />
+          <input
+            className="aboutInput"
+            type="email"
+            placeholder="Email"
+            name=""
+            id=""
+          />
+          <div className="w-40 bg-white h-11 rounded-full">
+            <HomeBtn text="Contact Us" />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default About
