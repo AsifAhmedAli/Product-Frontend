39543da3ae82138ea6f86e846f986f305ea7bb29
MahumSamar <msamar.bscs19seecs@seecs.edu.pk>
2021-12-18 20:12:34 +0500

    buttons

--- src/elements/FormInput.js
+++ src/elements/FormInput.js
@@ -1,20 +1,32 @@
-import styles from '../pages/auth/Auth.module.css';
+import styles from "../pages/auth/Auth.module.css"
 
-export default function FormInput({ type, name, placeholder, value, onChange, onBlur, errors }) {
-    return (
-        <>
-            <input
-                type={type}
-                name={name && name}
-                value={value && value}
-                className={`${styles.login_input} transition-all bg-transparent duration-300 py-2 placeholder-black focus:placeholder-transparent border-gray-400 border-0 border-b-2 w-full text-black outline-none focus:border-gray-900 text-sm my-3`}
-                placeholder={placeholder && placeholder}
-                onChange={onChange && onChange}
-                onBlur={onBlur && onBlur}
-            />
-            <div style={{ color: "red", fontSize: ".8rem" }}>
-                {errors && errors}
-            </div>
-        </>
-    )
+export default function FormInput({
+  type,
+  name,
+  placeholder,
+  value,
+  onChange,
+  onBlur,
+    errors,
+  icon
+}) {
+  return (
+    <div>
+      <div className="flex items-center justify-between border-blue-900 border-0 border-b-2 space-x-4 my-6">
+        <div>
+          <i className={`${icon} textColorBlue`}></i>
+        </div>
+        <input
+          type={type}
+          name={name && name}
+          value={value && value}
+          className={`${styles.login_input} transition-all bg-transparent duration-300 py-2 placeholder-blue-900 focus:placeholder-transparent w-full text-black outline-none  text-sm`}
+          placeholder={placeholder && placeholder}
+          onChange={onChange && onChange}
+          onBlur={onBlur && onBlur}
+        />
+      </div>
+      <div style={{ color: "red", fontSize: ".8rem" }}>{errors && errors}</div>
+    </div>
+  )
 }
--- src/elements/HomeBtn.js
+++ src/elements/HomeBtn.js
@@ -1,28 +1,17 @@
-// export default function HomeBtn({ text, white, full }) {
-//     return (
-//       <button
-//         className={`py-3 px-14 text-sm rounded-lg bg-transparent border-white border-opacity-50 border-2 transition-colors mb-4 ${
-//           full && `w-4/5`
-//         } lg:mb-0 ${
-//           white
-//             ? "bg-white text-gray-800 hover:bg-transparent hover:text-white"
-//             : "hover:bg-white hover:text-black"
-//         }`}
-//       >
-//         {text}
-//       </button>
-//     )
-// }
 function HomeBtn({ text, background }) {
   return (
     <div>
       {background == "blue" ? (
-        <div className="gradientBlueBtn flex items-center space-x-4 cursor-pointer py-1">
+        <div className="gradientBlueBtn flex items-center space-x-4 cursor-pointer py-1 mb-2 w-max">
           <p>{text}</p>
           <i className="far fa-paper-plane textColorBlue p-2 rounded-full bg-white" />
         </div>
       ) : (
-        <div className={`${background =="white" ? "bg-white":""} transparentBtn textColorBlue flex items-center space-x-4 cursor-pointer py-1`}>
+        <div
+          className={`${
+            background == "white" ? "bg-white" : ""
+          } transparentBtn textColorBlue flex items-center space-x-4 cursor-pointer py-1 mb-2 w-max`}
+        >
           <p>{text}</p>
           <i className="far fa-paper-plane text-white p-2 rounded-full bg-blue-900" />
         </div>
