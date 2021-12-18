231ea39537fec8156340242bd92069547537993f
MahumSamar <msamar.bscs19seecs@seecs.edu.pk>
2021-12-18 20:11:49 +0500

    dashboard front page

--- src/components/dashboard/DashboardBars.js
+++ src/components/dashboard/DashboardBars.js
@@ -1,27 +1,40 @@
-import Navbar from './Navbar';
-import Sidebar from './Sidebar';
-import { useEffect, useState } from 'react';
-import Welcome from './Welcome';
-import styles from '../../pages/dashboard/Dashboard.module.css';
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+import Welcome from "./Welcome"
+// import { useEffect, useState } from 'react';
+// import styles from '../../pages/dashboard/Dashboard.module.css';
 
-export default function DashboardBars({ children }) {
-    const [route, setRoute] = useState('dashboard');
-    const [toggleSidebar, setToggleSidebar] = useState(typeof window !== 'undefined' && window.matchMedia("(max-width: 640px)").matches);
+// export default function DashboardBars({ children }) {
+//     const [route, setRoute] = useState('dashboard');
+//     const [toggleSidebar, setToggleSidebar] = useState(typeof window !== 'undefined' && window.matchMedia("(max-width: 640px)").matches);
 
-    useEffect(() => {
-        window.matchMedia("(max-width: 640px)").addEventListener('change', e => {
-            setToggleSidebar(e.matches);
-        });
-    }, [])
+//     useEffect(() => {
+//         window.matchMedia("(max-width: 640px)").addEventListener('change', e => {
+//             setToggleSidebar(e.matches);
+//         });
+//     }, [])
 
-    return (
-        <div>
-            <Navbar toggleSidebar={toggleSidebar} setToggleSidebar={setToggleSidebar} />
-            <Sidebar setRoute={setRoute} route={route} toggleSidebar={toggleSidebar} setToggleSidebar={setToggleSidebar} />
-            <div className={`${styles.content}`}>
-                <Welcome />
-                {children}
-            </div>
-        </div>
-    )
+//     return (
+//         <div>
+//             <Navbar toggleSidebar={toggleSidebar} setToggleSidebar={setToggleSidebar} />
+//             {/* <Sidebar setRoute={setRoute} route={route} toggleSidebar={toggleSidebar} setToggleSidebar={setToggleSidebar} />
+//             <div className={`${styles.content}`}>
+//                 <Welcome />
+//                 {children}
+//             </div> */}
+//         </div>
+//     )
+// }
+function DashboardBars() {
+  return (
+    <div>
+      <Navbar />
+      <div className="flex space-x-6">
+        <Sidebar />
+        <Welcome />
+      </div>
+    </div>
+  )
 }
+
+export default DashboardBars
